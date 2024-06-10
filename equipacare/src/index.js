ac6a5a9edb08e5b14bc3fafc4050833a89277776
Vinicius-Coradini <vinicius.coradini@outlook.com>
2024-06-10 14:54:47 -0300

    First screen

--- equipacare/src/index.js
+++ equipacare/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+
+ReactDOM.render(
+  <Router>
+    <Navbar />
     <App />
-  </React.StrictMode>
+    <Footer />
+  </Router>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
